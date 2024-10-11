766932c68543057dd4fafc89b2168fef503e5200
pmorenateide <pmorena@teideformacion.com>
2024-10-11 17:47:10 +0200

    primerCommit

--- repo111024Spring/repo111024Spring/src/main/java/com/example/repo111024Spring/Repo111024SpringApplication.java
+++ repo111024Spring/repo111024Spring/src/main/java/com/example/repo111024Spring/Repo111024SpringApplication.java
@@ -3,11 +3,13 @@ package com.example.repo111024Spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Repo111024SpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Repo111024SpringApplication.class, args);
+//		SpringApplication.run(Repo111024SpringApplication.class, args);
+
+		System.out.println("GRACIAS POR NO IR A LA HUELGA");
 	}
 
 }
