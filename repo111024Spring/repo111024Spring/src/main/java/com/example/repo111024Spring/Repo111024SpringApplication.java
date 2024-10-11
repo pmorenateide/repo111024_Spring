879a7de45e6756004fb345c9c5817dd13749eb84
pmorenateide <pmorena@teideformacion.com>
2024-10-11 17:44:19 +0200

    Add files via upload

---
+++ repo111024Spring/repo111024Spring/src/main/java/com/example/repo111024Spring/Repo111024SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.repo111024Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Repo111024SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Repo111024SpringApplication.class, args);
+	}
+
+}
---
+++ repo111024Spring/repo111024Spring/src/test/java/com/example/repo111024Spring/Repo111024SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repo111024Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Repo111024SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
